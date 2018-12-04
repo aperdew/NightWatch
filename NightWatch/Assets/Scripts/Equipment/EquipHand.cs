1ae10a989c6837b45dbcb45364c68aa12d4ecc95
aperdew <perdewaaron@gmail.com>
2018-12-03 21:34:41 -0600

    Intial commit

---
+++ NightWatch/Assets/Scripts/Equipment/EquipHand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipHand : MonoBehaviour {
+     public GameObject hand;
+    public GameObject gun;
+	// Use this for initialization
+	void Start () {Instantiate(gun, hand.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ NightWatch/Assets/Scripts/Equipment/Weapons/FireGun.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour
+{
+
+    public GameObject projectile;
+    GameObject fire;
+    GameObject gameProjectile;
+    int timer;
+    int delay;
+
+    // Use this for initialization
+    void Start()
+    {
+        delay = 30;
+        timer = delay;
+        fire = transform.Find("Fire").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timer == delay)
+        {
+            timer = 0;
+            Fire();
+        }
+        else
+        {
+            timer++;
+        }
+    }
+
+    void Fire()
+    {
+        gameProjectile = Instantiate(projectile, fire.transform.position, fire.transform.rotation);
+        
+        gameProjectile.GetComponent<Rigidbody>().AddRelativeForce(fire.transform.forward * 150);
+        gameProjectile.transform.Rotate(Vector3.left * -90);
+        Destroy(gameProjectile, 5f);
+    }
+}
